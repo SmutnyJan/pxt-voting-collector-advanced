f32f2077524141855e12130406a7cf414ff74256
SmutnyJan <57211415+SmutnyJan@users.noreply.github.com>
2022-02-16 17:55:58 +0100

    finished

--- main.ts
+++ main.ts
@@ -1,1 +1,34 @@
-
+input.onButtonPressed(Button.A, function () {
+    odpovedi = Server.GetOdpovedi()
+    pocetD = 0
+    pocetC = 0
+    pocetB = 0
+    pocetA = 0
+    for (let hodnota of odpovedi) {
+        if (hodnota == "A") {
+            pocetA += 1
+        } else if (hodnota == "B") {
+            pocetB += 1
+        } else if (hodnota == "C") {
+            pocetC += 1
+        } else if (hodnota == "D") {
+            pocetD += 1
+        }
+    }
+    basic.showString("A" + pocetA)
+    basic.showString("B" + pocetB)
+    basic.showString("C" + pocetD)
+    basic.showString("D" + pocetC)
+})
+input.onButtonPressed(Button.B, function () {
+    Server.NoveHlasovani()
+})
+radio.onReceivedValue(function (name, value) {
+    Server.ZaznamenatHlas(name, value)
+})
+let pocetA = 0
+let pocetB = 0
+let pocetC = 0
+let pocetD = 0
+let odpovedi: string[] = []
+radio.setGroup(1)
---
+++ server.ts
@@ -0,0 +1,60 @@
+/**
+ * Použijte tento soubor k definování personalizovaných funkcí a bloků.
+ * Přečtěte si více na https://makecode.microbit.org/blocks/custom
+ */
+
+
+/**
+ * Custom blocks
+ */
+//% weight=100 color=#3bccc0 icon="\uf233"
+namespace Server {
+
+    let serialNumbers: number[] = []
+    let answers: string[] = []
+
+
+
+
+    /**
+    * Spustí nové hlasování a smaže uložená data
+    */
+    //% block="Spusť nové hlasování"
+
+    export function NoveHlasovani(): void {
+        answers = []
+        serialNumbers = []
+    }
+
+    /**
+    * Zaznamená nový hlas
+    */
+    //% block="Zaznamenej hlas %hlas se seriovým číslem %serioveCislo"
+
+    export function ZaznamenatHlas(hlas: string, serioveCislo: number): void {
+        let contains = false
+        for (let number of serialNumbers) {
+            if (number == serioveCislo) {
+                contains = true
+            }
+        }
+        if (contains == false) {
+            answers.push(hlas)
+            serialNumbers.push(serioveCislo)
+        } else {
+            answers[serialNumbers.indexOf(serioveCislo)] = hlas
+        }
+    }
+
+    /**
+    * Vrátí pole odpovědí
+    */
+    //% block="Získej odpovědi"
+
+    export function GetOdpovedi(): string[] {
+        return answers
+    }
+
+
+
+}
